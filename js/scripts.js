59885e0df2e60a6bf4693be238bc1bc122341110
Sam Jewell <sam.jewell@cantab.net>
2015-05-23 18:19:42 +0100

    Added all the things - added moment and lodash, mapped deflator to daily data, and found FTSE in real terms

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-$.getJSON("js/GBR_NGDP_D.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
-
-$.getJSON("js/INDEX_FTSE.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
